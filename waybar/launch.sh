a4d54f31b3a189c65e959ce7caa2afd5a012e8c7
Isaac Zamora <zackfall@outlook.com>
2025-02-18 13:51:31 -0500

    solving issue

--- waybar/launch.sh
+++ waybar/launch.sh
@@ -4,7 +4,7 @@ killall waybar
 
 if [[ $USER = "noir" ]]
 then
-  waybar -c ~/dotconfig/waybar/config & -s ~/dotconfig/waybar/style.css
+  waybar -c ~/dotconfig/waybar/config.jsonc & -s ~/dotconfig/waybar/style.css
 else
   waybar &
 fi
